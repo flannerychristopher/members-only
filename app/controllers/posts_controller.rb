097f011ee8155a3e96b45f1db55e64f2440324dc
Chris Flan <flanthedev@gmail.com>
2017-06-19 14:46:55 +1000

    finish post submission with anonymous author if not signed in

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -5,4 +5,36 @@ class PostsController < ApplicationController
     @posts = Post.all
   end
 
+  def new
+    @post = Post.new
+  end
+
+  def create
+    @post = Post.new(post_params)
+    @post.user_id = current_user.id
+
+    if @post.save
+      redirect_to posts_path
+    else
+      render 'new'
+    end
+
+  end
+
+
+
+  private
+
+    def user_logged_in
+      unless logged_in?
+        flash[:danger] = "Sign in first"
+        redirect_to signin_url
+      end
+    end
+
+    def post_params
+      params.require(:post).permit(:title, :body, :user_id)
+    end
+
+
 end
